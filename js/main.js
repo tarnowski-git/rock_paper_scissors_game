ad08dd1f8c4aa7a3a29e8f3d99472ffc1c9a2528
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-09 17:32:16 +0100

    Add functional User Interface

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ const scoreboard = {
     computer: 0
 };
 
-// Play game
+// Play game (main function)
 function play(e) {
     restart.style.display = "inline-block";
     const playerChoice = e.target.id;
     const computerChoice = getComputerChoice();
     const winner = getWinner(playerChoice, computerChoice);
-    console.log(playerChoice, computerChoice, winner);
+    showWinner(winner, computerChoice);
 }
 
 // Get computer choice
@@ -50,5 +50,48 @@ function getWinner(p, c) {
     }
 }
 
+function showWinner(winner, computerChoice) {
+    if (winner == "player") {
+        // Incrase player score
+        scoreboard.player++;
+        // Show modal result
+        result.innerHTML = `
+            <h1 class="text-win">You Win</h1>
+            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer chose <strong>${computerChoice}</strong></p>
+        `;
+    } else if (winner == "computer") {
+        // Incrase computer score
+        scoreboard.computer++;
+        // Show modal result
+        result.innerHTML = `
+            <h1 class="text-lose">You Lose</h1>
+            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer chose <strong>${computerChoice}</strong></p>
+        `;
+    } else {
+        // Show modal result
+        result.innerHTML = `
+            <h1>It's A Draw</h1>
+            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer chose <strong>${computerChoice}</strong></p>
+        `;
+    }
+    // Show score in modal
+    score.innerHTML = `
+        <p>Player: ${scoreboard.player}</p>
+        <p>Computer: ${scoreboard.computer}</p>
+    `;
+    modal.style.display = "block";
+}
+
+// Clear modal
+function clearModal(e) {
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 // Event listeners
 choices.forEach(choice => choice.addEventListener("click", play));
+window.addEventListener("click", clearModal);
