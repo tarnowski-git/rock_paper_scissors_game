79868cf3e548baaf8c0d81744d7ef6ec57793578
Konrad Tarnowski <k.tarnowski@student.uksw.edu.pl>
2020-01-09 01:59:19 +0100

    Add simple game interface

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const choices = document.querySelectorAll(".choice");
+const score = document.getElementById("score");
+const result = document.getElementById("result");
+const restart = document.getElementById("restart");
+const modal = document.querySelector(".modal");
+const scoreboard = {
+    player: 0,
+    computer: 0
+};
+
+// Play game
+function play(e) {
+    restart.style.display = "inline-block";
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+
+    console.log(playerChoice, computerChoice);
+}
+
+// Get computer choice
+function getComputerChoice() {
+    const rand = Math.random();
+    if (rand < 0.34) {
+        return "rock";
+    } else if (rand <= 0.67) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+// Event listeners
+choices.forEach(choice => choice.addEventListener("click", play));
